9ce3d81c0a7bd2aa4de4d427911e1204331e97d7
Usha Sivakumar <91612401+sk2607@users.noreply.github.com>
2026-03-20 10:54:39 -0400

    Update basics.sql
    sql-- My first SQL queries
    
    -- Select all data from a table
    SELECT * FROM employees;
    
    -- Filter data
    SELECT name, salary
    FROM employees
    WHERE salary > 50000;
    
    -- Sort data
    SELECT name, salary
    FROM employees
    ORDER BY salary DESC;

--- basics.sql
+++ basics.sql
@@ -1,1 +1,14 @@
-basics.sql
+sql-- My first SQL queries
+
+-- Select all data from a table
+SELECT * FROM employees;
+
+-- Filter data
+SELECT name, salary
+FROM employees
+WHERE salary > 50000;
+
+-- Sort data
+SELECT name, salary
+FROM employees
+ORDER BY salary DESC
